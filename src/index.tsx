36206e594910e129d31b7668f4a81bab03b69fcd
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-06-20 14:25:23 -0700

    added draggable functionality

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 // 1) Import ReactDOM library
 import ReactDOM from "react-dom/client";
 
-import UserSearch from './state/UserSearch';
+import EventComponent from "./events/EventComponent";
  
 // 2) Get a reference to the div with ID root
 const el = document.getElementById("root");
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(el!);
 const App = () => {
   return (
     <div>
-      <UserSearch />
+      <EventComponent />
     </div>
   );
 };
