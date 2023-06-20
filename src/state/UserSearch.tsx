008f0310b9f903c8f1e6652f26962b4f62c4e4d7
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-06-20 14:03:52 -0700

    replaced console log of found user with rendering on browser

--- src/state/UserSearch.tsx
+++ src/state/UserSearch.tsx
@@ -1,27 +1,34 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const users = [
-  { name: 'Sarah', age: 20 },
-  { name: 'Alex', age: 20 },
-  { name: 'Michael', age: 20 }
+  { name: "Sarah", age: 20 },
+  { name: "Alex", age: 20 },
+  { name: "Michael", age: 20 },
 ];
 
 const UserSearch: React.FC = () => {
-  const [name, setName] = useState('');
+  const [name, setName] = useState("");
+  const [user, setUser] = useState<{ name: string; age: number } | undefined>();
 
   const onClick = () => {
     const foundUser = users.find((user) => {
       return user.name === name;
     });
 
-    console.log(foundUser);
+    setUser(foundUser);
   };
 
-  return <div>
-    User Search
-    <input value={name} onChange={e => setName(e.target.value)} />
-    <button onClick={onClick}>Find User</button>
+  return (
+    <div>
+      User Search
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <button onClick={onClick}>Find User</button>
+      <div>
+        {user && user.name}
+        {user && user.age}
+      </div>
     </div>
+  );
 };
 
 export default UserSearch;
