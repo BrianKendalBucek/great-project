5c161446880532ab4d034aaedcde223119f31a18
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-06-20 13:58:05 -0700

    added user search functionality

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 // 1) Import ReactDOM library
 import ReactDOM from "react-dom/client";
 
-import GuestList from './state/GuestList';
+import UserSearch from './state/UserSearch';
  
 // 2) Get a reference to the div with ID root
 const el = document.getElementById("root");
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(el!);
 const App = () => {
   return (
     <div>
-      <GuestList />
+      <UserSearch />
     </div>
   );
 };
